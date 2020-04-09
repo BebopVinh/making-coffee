84936ddf05b1ac50e213206c10444e3279f4f627
bebopvinh <vinhdo07@gmail.com>
2020-04-09 11:02:51 -0700

    remove boilerplate slides, add intro slide

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    name: `Fabian Schultz`,
-    title: `Gatsby Deck`,
-    date: `July 30, 2018`,
+    name: `Vinh Do`,
+    title: `Making Coffee`,
+    date: `2020`,
   },
   plugins: [
     `gatsby-plugin-layout`,
